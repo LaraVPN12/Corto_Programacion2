189fd6bceab56f6fc513fab9445e3bbe8d517cf8
Jose Sandoval <48607722+SM18003@users.noreply.github.com>
2019-04-02 20:22:47 -0600

    APP
    Fusionando todo :bikini:

---
+++ src/registro/Registro.java
@@ -0,0 +1,11 @@
+package registro;
+import javax.swing.*;
+public class Registro {
+
+    public static void main(String[] args) {
+    Ventana_Registro ventana = new Ventana_Registro();
+    ventana.setTitle("Registro");
+    ventana.setVisible(true);
+    }
+    
+}
